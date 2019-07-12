73af0482da8990144966f0100f4560722d611d52
M Daffa <41532495+md4fv@users.noreply.github.com>
2019-07-12 18:14:31 +0700

    Update py3.py

--- py3.py
+++ py3.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 #-*- coding: utf-8 -*-
+#marshal py3
 
 '''
 PyMarshal - Compile Python Script
@@ -25,38 +26,49 @@ w='\033[1;37m'
 n='\033[0;00m' 
 br='\033[91;7m' 
 
-bannerpy2 = """
+bannerpy3 = """
          {}___ 
-{} ___ ___|{}_  {}{}| {}Author  {}:{} Dfv47
-{}| . | | |{}  _{}{}| {}Code    {}:{} Python
+{} ___ _  |{}_  {}{}| {}Author  {}:{} Dfv47
+{}| . | | |{}_  {}{}| {}Code    {}:{} Python
 {}|  _|_  |{}___{}{}| {}Version {}:{} v.5.0
-{}|_| |___| {}*{} https://github.com/md4fv   
+{}|_| |___| {}*{} https://github.com/md4fv  
 """.format(r,y,br,n,y,w,r,w,y,br,n,y,w,r,w,y,br,n,y,w,r,w,y,r,a)
+
+'''
+Coded  : @m_d4fv
+Author : Dfv47
+Team   : Black Coder Crush
+Phone  : 6282223108828
+Email  : daffamfthhsn21@gmail.com
+Thanks : ZoneExploiter & CytoXploit
+'''
 
 os.system('clear')
 try:
-    print(bannerpy2)
+    print(bannerpy3)
     print (y+' ['+w+'#'+y+'] '+w+'Example '+y+':'+w+' /sdcard/dfv.py')
     file = input(y+' ['+w+'?'+y+'] '+w+'Input your file location'+y+' :'+w+' ')
-    dfv = file.replace('.py', '')
+    o = file.replace('.py', '')
 except KeyboardInterrupt:
     sys.exit()
 else:
     try:
-       strng = open(file, 'r').read()
+        strng = open(file, 'r').read()
     except IOError:
-       print (r+'\n ['+w+'!'+r+'] '+r+'[ '+w+'Error '+r+'] '+w+'No such file or directory '+r+': '+w+'"'+dfv+'"\n')
-       sys.exit()
+        print (r+'\n ['+w+'!'+r+'] '+r+'[ '+w+'Error '+r+'] '+w+'No such file or directory '+r+': '+w+'"'+dfv+'"\n')
+        sys.exit()
     try:
         code = compile(strng,'','exec')
         data = marshal.dumps(code)
     except TypeError:
-       sys.exit()
-fileout = open(dfv +'enc.py', 'wb')
+        print (R + '   ['+W+'!'+R+'] '+R+'[ '+W+'File already to compiled\n') 
+        sys.exit()
+
+fileout = open(o + 'enc.py', 'w')
 fileout.write('#Compiled By DfvTools\n')
 fileout.write('#https://github.com/md4fv\n')
 fileout.write('import marshal\n')
-fileout.write('exec(marshal.loads('+repr(data)+'))')
+fileout.write('exec(marshal.loads(' + repr(data) + '))')
 fileout.close()
 time.sleep(3) 
-print (y+'\n ['+w+'+'+y+'] '+w+'File succes to compile   '+y+': ' + w + dfv + 'enc.py\n')
+print (y+'\n ['+w+'+'+y+'] '+w+'File succes to compile   '+y+': ' + w + o + 'enc.py\n')
